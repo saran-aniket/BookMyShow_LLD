ed4c486ac50780ea80a1aae164079673f552e751
Aniket Sharan <sarananiketgupta1610@gmail.com>
2024-12-31 17:20:24 +0530

    do initial commit

---
+++ src/main/java/com/lldbackend/bms_lld_123124/BmsLld123124Application.java
@@ -0,0 +1,13 @@
+package com.lldbackend.bms_lld_123124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsLld123124Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsLld123124Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lldbackend/bms_lld_123124/BmsLld123124ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lldbackend.bms_lld_123124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsLld123124ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
